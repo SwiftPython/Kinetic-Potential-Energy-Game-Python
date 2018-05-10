61c7bc0cddabbe9b67da51fe30656d6d9c62e6a2
SwiftPython <33144708+SwiftPython@users.noreply.github.com>
2018-05-09 22:36:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import random
 score=0
 z=1
 while z==(1):
-    a=random.randint(1, 99999999)
+    a=random.randint(1, 100)
     b=random.randint(1,2)
-    c=random.randint(1, 99999999)
+    c=random.randint(1, 100)
     mass=str(a) + ("kilograms=mass")
     if b==(1):
         speed=str(c) + ("m/s=speed")
@@ -15,9 +15,9 @@ while z==(1):
         if q1==(speed2):
             score+=1
             print ("Yay! you scored a point! Your current score is %s." % score)
-        elif q1!=(speed2):
-            score=0
+        else:
             print ("Game over! Your score was %s!" % score)
+            score=0
     elif b==(2):
         height=str(c) + ("meters")
         print (height)
@@ -27,6 +27,6 @@ while z==(1):
         if q1==(answer):
             score+=1
             print ("Yay! you scored a point! Your current score is %s." % score)
-        elif q1!=(answer):
-            score=0
+        else:
             print ("Game over! Your score was %s!" % score)
+            score=0
